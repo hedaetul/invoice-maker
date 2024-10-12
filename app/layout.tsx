e257398dff1fbb107700f48fb5e9cb45439b2945
hedaetul <hedaetulislam143@gmail.com>
2024-10-12 16:35:58 +0600

    change layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,12 +3,15 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import { cn } from '@/lib/utils';
 import { InvoiceProvider } from './context/invoiceContext';
+import { link } from 'fs';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'invoice-make',
   description: 'Transforming bills into brilliance...',
+  keywords: 'invoice, billing, finance, invoice maker, invoice generator',
+  authors: [{ name: 'Hedaetul Islam', url: 'https://github.com/hedaetul' }],
 };
 
 export default function RootLayout({
@@ -18,6 +21,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
+      <head>
+         <link rel="icon" href="/favicon.ico" sizes="xl" />
+      </head>
       <InvoiceProvider>
         <body className={cn(inter.className)}>
           {children}
